7a47d26777d917bb7cb5ca9deb95dac5074b4a98
Joe Fitzgerald <jfitzgerald@pivotal.io>
2017-12-20 10:06:11 -0700

    Rip out files

---
+++ graphql.go
@@ -0,0 +1,168 @@
+// Package graphql provides a low level GraphQL client.
+//
+//  // create a client (safe to share across requests)
+//  client := graphql.NewClient("https://machinebox.io/graphql")
+//
+//  // make a request
+//  req := graphql.NewRequest(`
+//      query ($key: String!) {
+//          items (id:$key) {
+//              field1
+//              field2
+//              field3
+//          }
+//      }
+//  `)
+//
+//  // set any variables
+//  req.Var("key", "value")
+//
+//  // run it and capture the response
+//  var respData ResponseStruct
+//  if err := client.Run(ctx, req, &respData); err != nil {
+//      log.Fatal(err)
+//  }
+//
+// Specify client
+//
+// To specify your own http.Client, use the WithHTTPClient option:
+//  httpclient := &http.Client{}
+//  client := graphql.NewClient("https://machinebox.io/graphql", graphql.WithHTTPClient(httpclient))
+package graphql
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"mime/multipart"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+// Client is a client for accessing a GraphQL dataset.
+type Client struct {
+	endpoint   string
+	httpClient *http.Client
+}
+
+// NewClient makes a new Client capable of making GraphQL requests.
+func NewClient(endpoint string, opts ...ClientOption) *Client {
+	c := &Client{
+		endpoint: endpoint,
+	}
+	for _, optionFunc := range opts {
+		optionFunc(c)
+	}
+	if c.httpClient == nil {
+		c.httpClient = http.DefaultClient
+	}
+	return c
+}
+
+// Run executes the query and unmarshals the response from the data field
+// into the response object.
+// Pass in a nil response object to skip response parsing.
+// If the request fails or the server returns an error, the first error
+// will be returned.
+func (c *Client) Run(ctx context.Context, req *Request, resp interface{}) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+	if err := writer.WriteField("query", req.Query); err != nil {
+		return errors.Wrap(err, "write query field")
+	}
+	if len(req.Variables) > 0 {
+		variablesField, err := writer.CreateFormField("variables")
+		if err != nil {
+			return errors.Wrap(err, "create variables field")
+		}
+		if err := json.NewEncoder(variablesField).Encode(req.Variables); err != nil {
+			return errors.Wrap(err, "encode variables")
+		}
+	}
+	if err := writer.Close(); err != nil {
+		return errors.Wrap(err, "close writer")
+	}
+	var graphResponse = struct {
+		Data   interface{}
+		Errors []graphErr
+	}{
+		Data: resp,
+	}
+
+	r, err := http.NewRequest(http.MethodPost, c.endpoint, &requestBody)
+	if err != nil {
+		return err
+	}
+	r.Header.Set("Content-Type", writer.FormDataContentType())
+	r.Header.Set("Accept", "application/json")
+	r = r.WithContext(ctx)
+	res, err := c.httpClient.Do(r)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, res.Body); err != nil {
+		return errors.Wrap(err, "reading body")
+	}
+	if err := json.NewDecoder(&buf).Decode(&graphResponse); err != nil {
+		return errors.Wrap(err, "decoding response")
+	}
+	if len(graphResponse.Errors) > 0 {
+		// return first error
+		return graphResponse.Errors[0]
+	}
+	return nil
+}
+
+// WithHTTPClient specifies the underlying http.Client to use when
+// making requests.
+//  NewClient(endpoint, WithHTTPClient(specificHTTPClient))
+func WithHTTPClient(httpclient *http.Client) ClientOption {
+	return ClientOption(func(client *Client) {
+		client.httpClient = httpclient
+	})
+}
+
+// ClientOption are functions that are passed into NewClient to
+// modify the behaviour of the Client.
+type ClientOption func(*Client)
+
+type graphErr struct {
+	Message string
+}
+
+func (e graphErr) Error() string {
+	return "graphql: " + e.Message
+}
+
+// Request is a GraphQL request.
+type Request struct {
+	OperationName string                 `json:"operationName,omitempty"`
+	Query         string                 `json:"query"`
+	Variables     map[string]interface{} `json:"variables,omitempty"`
+}
+
+// NewRequest makes a new Request with the specified string.
+func NewRequest(q string) *Request {
+	req := &Request{
+		Query: q,
+	}
+	return req
+}
+
+// Var sets a variable.
+func (req *Request) Var(key string, value interface{}) {
+	if req.Variables == nil {
+		req.Variables = make(map[string]interface{})
+	}
+	req.Variables[key] = value
+}
---
+++ graphql_test.go
@@ -0,0 +1,157 @@
+package graphql
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestWithClient(t *testing.T) {
+	RegisterTestingT(t)
+	var calls int
+	testClient := &http.Client{
+		Transport: roundTripperFunc(func(req *http.Request) (*http.Response, error) {
+			calls++
+			resp := &http.Response{
+				Body: ioutil.NopCloser(strings.NewReader(`{"data":{"key":"value"}}`)),
+			}
+			return resp, nil
+		}),
+	}
+
+	ctx := context.Background()
+	client := NewClient("", WithHTTPClient(testClient))
+
+	req := NewRequest(``)
+	client.Run(ctx, req, nil)
+
+	Expect(calls).Should(Equal(1))
+}
+
+func TestDo(t *testing.T) {
+	RegisterTestingT(t)
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		Expect(r.Method).Should(Equal(http.MethodPost))
+		query := r.FormValue("query")
+		Expect(query).Should(Equal(`query {}`))
+		io.WriteString(w, `{
+			"data": {
+				"something": "yes"
+			}
+		}`)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	client := NewClient(srv.URL)
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	var responseData map[string]interface{}
+	err := client.Run(ctx, &Request{Query: "query {}"}, &responseData)
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(calls).Should(Equal(1))
+	Expect(responseData["something"]).Should(Equal("yes"))
+}
+
+func TestDoErr(t *testing.T) {
+	RegisterTestingT(t)
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		Expect(r.Method).Should(Equal(http.MethodPost))
+		query := r.FormValue("query")
+		Expect(query).Should(Equal(`query {}`))
+		io.WriteString(w, `{
+			"errors": [{
+				"message": "Something went wrong"
+			}]
+		}`)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	client := NewClient(srv.URL)
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	var responseData map[string]interface{}
+	err := client.Run(ctx, &Request{Query: "query {}"}, &responseData)
+	Expect(err).ShouldNot(BeNil())
+	Expect(err.Error()).Should(Equal("graphql: Something went wrong"))
+}
+
+func TestDoNoResponse(t *testing.T) {
+	RegisterTestingT(t)
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		Expect(r.Method).Should(Equal(http.MethodPost))
+		query := r.FormValue("query")
+		Expect(query).Should(Equal(`query {}`))
+		io.WriteString(w, `{
+			"data": {
+				"something": "yes"
+			}
+		}`)
+	}))
+	defer srv.Close()
+
+	ctx := context.Background()
+	client := NewClient(srv.URL)
+
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+	err := client.Run(ctx, &Request{Query: "query {}"}, nil)
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(calls).Should(Equal(1))
+}
+
+func TestQuery(t *testing.T) {
+	RegisterTestingT(t)
+
+	var calls int
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		calls++
+		query := r.FormValue("query")
+		Expect(query).Should(Equal("query {}"))
+		Expect(r.FormValue("variables")).Should(Equal(`{"username":"tester"}` + "\n"))
+		_, err := io.WriteString(w, `{"data":{"value":"some data"}}`)
+		Expect(err).ShouldNot(HaveOccurred())
+	}))
+	defer srv.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	client := NewClient(srv.URL)
+
+	req := NewRequest("query {}")
+	req.Var("username", "tester")
+
+	// check variables
+	Expect(req).ShouldNot(BeNil())
+	Expect(req.Variables["username"]).Should(Equal("tester"))
+
+	var resp struct {
+		Value string
+	}
+	err := client.Run(ctx, req, &resp)
+	Expect(err).ShouldNot(HaveOccurred())
+	Expect(calls).Should(Equal(1))
+	Expect(resp.Value).Should(Equal("some data"))
+}
+
+type roundTripperFunc func(req *http.Request) (*http.Response, error)
+
+func (fn roundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return fn(req)
+}
